b9670d0f49ad5fb9ae7fd84eaa807e8b21ddb3ef
MAStolbov <stolbov1108@yandex.ru>
2020-09-01 17:52:27 +0300

    Fix tests Add test with Roboletric

--- app/src/androidTest/java/com/example/android/trainingplanner/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/trainingplanner/ExampleInstrumentedTest.kt
@@ -1,17 +1,12 @@
 package com.example.android.trainingplanner
 
-import androidx.room.Room
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.database.TemplatesDatabase
-import com.example.android.database.templateEntityDao.TemplatesDatabaseDAO
-import com.example.android.database.templateEntityDao.TrainingTemplate
-import org.junit.After
-
+import com.example.android.repository.Repository
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,54 +14,4 @@ import org.junit.Before
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-
-    private lateinit var templateDao: TemplatesDatabaseDAO
-    private lateinit var db: TemplatesDatabase
-
-    @Before
-    fun createDb() {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-
-        db = Room.inMemoryDatabaseBuilder(context, TemplatesDatabase::class.java)
-            .allowMainThreadQueries()
-            .build()
-        templateDao = db.templateDatabaseDao
-    }
-
-    @After
-    fun closeDb() {
-        db.close()
-    }
-
-//    @Test
-//    fun insertAndGetTemplate(){
-//        val newTemplate = TrainingTemplate()
-//
-//        templateDao.insertTemplate(newTemplate)
-//
-//        val template = templateDao.getTemplate()
-//        newTemplate.templateId = 1
-//
-//        assertEquals(newTemplate,template)
-//    }
-
-    @Test
-    fun updateTemplate(){
-        val newTemplate = TrainingTemplate()
-        templateDao.insertTemplate(newTemplate)
-
-        //newTemplate.templateId = 1
-        newTemplate.numberOfTrainingWeeks = 1
-        newTemplate.templateName = "TestName1"
-        templateDao.updateTemplate(newTemplate)
-
-        //newTemplate.numberOfTrainingWeeks = 2
-
-        //val template = templateDao.getTemplate()
-
-        //assertEquals(newTemplate,template)
-    }
-
-
-}
+class ExampleInstrumentedTest
--- app/src/test/java/com/example/android/trainingplanner/ExampleUnitTest.kt
+++ app/src/test/java/com/example/android/trainingplanner/ExampleUnitTest.kt
@@ -1,21 +1,34 @@
 package com.example.android.trainingplanner
 
+
+
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.example.android.database.exerciseEntityDao.Exercise
+import com.example.android.repository.Repository
 import com.example.android.util.TemporaryDataStorageClass
+import org.hamcrest.core.IsNot.not
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = intArrayOf(24))
 class ExampleUnitTest {
     private lateinit var temporaryDataStorageClass: TemporaryDataStorageClass
+    private val context = ApplicationProvider.getApplicationContext<Context>()
 
     @Before
     fun getDataStorageInstance(){
@@ -23,37 +36,65 @@ class ExampleUnitTest {
     }
 
     @Test
+    fun repository_isCreated(){
+        val repository = Repository.getRepositoryInstance(context)
+    }
+
+
+    @Test
     fun exercises_isCreated() {
+
+        val exerciseName = "test"
+        val set = "2"
+        val rep = "3"
+        val weight = "20"
+        val weekNumber = 2
+        val dayNumber = 1
+        val localId = 1
+
         val fakeExercise = Exercise(
-            exerciseName = "test",
-            set = "2",
-            rep = "3",
-            weight = "20",
-            weekNumber = 2,
-            dayNumber = 1,
-            localId = 1
+            exerciseName = exerciseName,
+            set = set,
+            rep = rep,
+            weight = weight,
+            weekNumber = weekNumber,
+            dayNumber = dayNumber,
+            localId = localId
         )
 
         temporaryDataStorageClass.currentTrainingDay.apply {
-            weekNumber = 2
-            dayNumber = 1
+            this.weekNumber =  weekNumber
+            this.dayNumber = dayNumber
         }
 
-        temporaryDataStorageClass.createNewExercise("test", "2", "3", "20")
+        temporaryDataStorageClass.createNewExercise(exerciseName, set, rep, weight)
 
         assertEquals(fakeExercise,temporaryDataStorageClass.exercisesList.last())
+
+//        assertThat(fakeExercise,not(temporaryDataStorageClass.exercisesList.last()))
     }
 
     @Test
     fun returnExerciseListForSpecificDay_isTrue(){
-        val fakeExerciseList = listOf(Exercise(1L,weekNumber = 2,dayNumber = 2),Exercise(2L,weekNumber = 2,dayNumber = 2))
+
+        val secondWSecondDExerciseOne = Exercise(1L,weekNumber = 2,dayNumber = 2)
+        val secondWSecondDExerciseTwo = Exercise(2L,weekNumber = 2,dayNumber = 2)
+        val secondWThirdDExerciseOne = Exercise(3L,weekNumber = 2,dayNumber = 3)
+
+        val fakeExerciseList = listOf(secondWSecondDExerciseOne,secondWSecondDExerciseTwo)
 
         temporaryDataStorageClass.exercisesList.apply {
-            add(Exercise(1L,weekNumber = 2,dayNumber = 2))
-            add(Exercise(2L,weekNumber = 2,dayNumber = 2))
-            add(Exercise(3L,weekNumber = 2,dayNumber = 3))
+            add(secondWSecondDExerciseOne)
+            add(secondWSecondDExerciseTwo)
+            add(secondWThirdDExerciseOne)
         }
 
         assertEquals( fakeExerciseList,temporaryDataStorageClass.returnExerciseListForSpecificDay(2,2))
+    }
+
+
+    @After
+    fun clearData(){
+        temporaryDataStorageClass.exercisesList.clear()
     }
 }
